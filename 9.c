c9ef27742105dc027f655276724ea6e10b628fd1
PrakharAmity <145323910+PrakharAmity@users.noreply.github.com>
2023-09-26 19:41:44 +0530

    Add files via upload

---
+++ 9.c
@@ -0,0 +1,21 @@
+//program to calculate total and average marks
+#include <stdio.h>
+
+int main()
+{
+    int english, chemistry, computers, physics, maths; 
+    float Total, Average, Percentage;
+
+    printf("Please Enter the marks of five subjects: \n");
+    scanf("%d%d%d%d%d", &english, &chemistry, &computers, &physics, &maths);
+
+    Total = english + chemistry + computers + physics + maths;
+    Average = Total / 5;
+    
+
+    printf("Total Marks = %.2f\n", Total);
+    printf("Average Marks = %.2f\n", Average);
+    
+
+    return 0;
+}
