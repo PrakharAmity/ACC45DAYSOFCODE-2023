6e572c19fd45b6f3c06c4a9d6e510e3c422a2026
PrakharAmity <145323910+PrakharAmity@users.noreply.github.com>
2023-10-24 07:43:57 +0530

    Add files via upload

---
+++ 29.c
@@ -0,0 +1,37 @@
+//Program to check whether entered number is a strong number
+//Strong Number=Sum of factorial of digits is equal to the original number
+#include <stdio.h>
+ 
+int main()
+{
+  int Number, Temp, Reminder, Sum = 0, i; 
+  long Factorial;
+ 
+  printf("\nPlease Enter a Number to Check for Strong Number \n");
+  scanf("%d", &Number);
+  
+
+  Temp = Number;
+  while( Temp > 0)
+   {
+     Factorial = 1, i = 1; 
+     Reminder = Temp % 10;
+     while (i <= Reminder)
+     {
+     	Factorial = Factorial * i;
+     	i++;
+     }
+     printf("\n Factorial of %d = %d\n", Reminder, Factorial);
+     Sum = Sum + Factorial;
+     Temp = Temp /10;
+   }
+ 
+  printf("\n Sum of the Factorials of a Given Number %d is = %d\n", Number, Sum);
+ 
+  if ( Number == Sum )
+      printf("\n %d is a Strong Number.\n", Number);
+  else
+      printf("\n %d is not a Strong Number.\n", Number);
+ 
+  return 0;
+}
