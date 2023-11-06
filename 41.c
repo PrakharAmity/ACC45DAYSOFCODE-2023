4ef17c3356480b0c5c839e20682bca1d57c467d1
PrakharAmity <145323910+PrakharAmity@users.noreply.github.com>
2023-11-06 20:25:51 +0530

    Add files via upload

---
+++ 41.c
@@ -0,0 +1,32 @@
+//Number guessing game using c programming
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+int main() {
+    srand(time(NULL));
+
+    int secretNumber = rand() % 100 + 1;
+    
+    int guess;
+    int attempts = 0;
+    
+    printf("Welcome to the Number Guessing Game!\n");
+    printf("I'm thinking of a number between 1 and 100.\n");
+
+    do {
+        printf("Enter your guess: ");
+        scanf("%d", &guess);
+        attempts++;
+
+        if (guess < secretNumber) {
+            printf("Too low! Try again.\n");
+        } else if (guess > secretNumber) {
+            printf("Too high! Try again.\n");
+        } else {
+            printf("Congratulations! You guessed the number %d in %d attempts.\n", secretNumber, attempts);
+        }
+    } while (guess != secretNumber);
+
+return 0;
+}
